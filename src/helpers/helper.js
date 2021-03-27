19d2b2c03fef41cf01fb929454c08162e842d726
Ilyar <ilyar.software@gmail.com>
2021-03-27 22:50:04 +0200

    Add helper for data url

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -19,3 +19,13 @@ export function getTypeImageByNumber(type) {
       return '/images/store.svg';
   }
 }
+
+const ipfsDefaultGateways = 'ipfs.io'
+export function parseDataURL(raw) {
+  const url = new URL(raw);
+  switch (url.protocol) {
+    case 'ipfs:':
+    default:
+      return `https://${ipfsDefaultGateways}/ipfs/{${url.hostname}/`;
+  }
+}
