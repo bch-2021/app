bc1c09a7d8921e80d5de110666c42d0bbebbe5bb
Oleksandr Fedorenko <agoodminute@gmail.com>
2021-03-27 21:24:50 +0200

    Add example of searching

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,16 +1,31 @@
 import Grid from '@material-ui/core/Grid';
+import { useEffect, useState } from 'react';
 import Layout from '../Layout';
 import Input from '../../elements/Input';
 import Button1 from '../../elements/Button1';
 import cl from './style.module.scss';
 import HistoryItem from './HistoryItem';
+import axios from 'axios';
 
 export default function Search() {
+  const [searchNumber, setSearchNumber] = useState('');
+
+  useEffect(async () => {
+    console.log('Write here!');
+
+    let link = 'ipfs://QmNZzDacUbgYbSXJinEcNeBvkAePbUgRfme26rq5pmqstm';
+    link = 'https://ipfs.io/ipfs/QmdvSQPZ8YfkfrrNqf5ULNme2cwTzHFGGpN9UYMotoVCYe';
+
+    console.log(searchNumber);
+    // const resp = await axios({ method: 'get', url: link });
+    // resp.data
+  }, []);
+
   return (
     <Layout>
       <Grid container spacing={2}>
         <Grid item xs={10}>
-          <Input name="Serial number" />
+          <Input name="Serial number" value={searchNumber} onChange={(e) => setSearchNumber(e.target.value)} />
         </Grid>
         <Grid item xs={2}>
           <Button1 name="Search" />
